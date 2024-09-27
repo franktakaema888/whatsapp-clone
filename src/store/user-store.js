93626028d5052111bd97053191ff9e21c370af3b
franktakaema888 <marktakaema@gmail.com>
2024-09-27 16:52:15 +0800

    application setup

--- src/store/user-store.js
+++ src/store/user-store.js
@@ -1,15 +1,9 @@
 // stores/counter.js
 import { defineStore } from 'pinia'
 
-export const useCounterStore = defineStore('counter', {
-  state: () => {
-    return { count: 0 }
-  },
-  // could also be defined as
-  // state: () => ({ count: 0 })
+export const useUserStore = defineStore('user', {
+  state: () => ({  }),
   actions: {
-    increment() {
-      this.count++
-    },
+    // 
   },
 })
