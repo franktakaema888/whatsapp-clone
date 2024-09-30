b95d727be3e8d8637d4029d2e4e659079f1b4b14
franktakaema888 <marktakaema@gmail.com>
2024-09-30 14:31:00 +0800

    login page update

---
+++ src/views/LoginView.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class=""w-full>
+        <div class="bg-teal-600 z-[-1] w-full h-[255px] fixed top-0"></div>
+        <div class="bg-[#191919] z-[-1] w-full h-[calc(100vh-255px)] fixed bottom-0"></div>
+
+    </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style lang="scss" scoped>
+
+</style>
